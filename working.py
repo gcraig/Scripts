74c63415877c7d46a018bfb396da44a4d5ce7dc4
George Craig <georgeacraig@gmail.com>
2016-09-16 20:19:41 -0500

    add rudimentary "working ... *" status message

---
+++ working.py
@@ -0,0 +1,18 @@
+def print_working(delay):       
+     print "Working ... \\\r",  
+     time.sleep(delay) 
+     print "Working ... |\r",   
+     time.sleep(delay) 
+     print "Working ... /\r",   
+     time.sleep(delay) 
+     print "Working ... -\r",   
+     time.sleep(delay) 
+     print "Working ... \\\r",  
+     time.sleep(delay) 
+     print "Working ... |\r",   
+     time.sleep(delay) 
+     print "Working ... /\r",   
+     time.sleep(delay) 
+     print "Working ... -\r",   
+     time.sleep(delay) 
+     print "Working ... DONE"
